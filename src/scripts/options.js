da7dde7e453ef1f2e93da9d72dfd9385a8c61214
Rémi Robichet <remi.robichet@cgi.com>
2019-12-19 17:55:04 +0100

    :rocket: 1st commit

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -1,30 +1,40 @@
 import ext from "./utils/ext";
 import storage from "./utils/storage";
 
-var colorSelectors = document.querySelectorAll(".js-radio");
+var aBookml = [];
 
-var setColor = (color) => {
-  document.body.style.backgroundColor = color;
-};
-
-storage.get('color', function(resp) {
-  var color = resp.color;
-  var option;
-  if(color) {
-    option = document.querySelector(`.js-radio.${color}`);
-    setColor(color);
-  } else {
-    option = colorSelectors[0]
+storage.get(['bookml'], function(resp) {
+  if (resp.bookml) {
+    aBookml = resp.bookml;
+    renderList(aBookml);
   }
-
-  option.setAttribute("checked", "checked");
 });
 
-colorSelectors.forEach(function(el) {
-  el.addEventListener("click", function(e) {
-    var value = this.value;
-    storage.set({ color: value }, function() {
-      setColor(value);
-    });
-  })
+document.querySelector(".add-bookml").addEventListener("click", function() {
+  var title = document.querySelector(".new-bookml-title").value.trim();
+  var url = document.querySelector(".new-bookml-url").value.trim();
+  if (title !== '' && title !== null && title && url !== '' && url !== null && url) {
+    console.log({title, url});
+    aBookml.push({title, url});
+    storage.set({bookml: aBookml});
+    renderList(aBookml);
+  }
 })
+
+document.querySelector(".import-bookml").addEventListener("click", function() {
+  // @todo popin import fichier dans localstorage bookml
+})
+
+document.querySelector(".export-bookml").addEventListener("click", function() {
+  // @todo popin export dans localstorage bookml dans fichier
+  console.log(JSON.stringify(aBookml));
+})
+
+var renderList = (list) => {
+  console.log(list);
+  var elListItem = '';
+  list.forEach((value) => {
+    elListItem += `<li>${value.title}: ${value.url}</li>`
+  })
+  document.querySelector("ul.list").innerHTML = elListItem;
+}
