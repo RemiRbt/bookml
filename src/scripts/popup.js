4b32311901abd08358230df114bbc6121bd08ceb
Rémi Robichet <remi.robichet@cgi.com>
2019-12-20 17:22:22 +0100

    :recycle: Refacto gist call

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -2,14 +2,14 @@ import ext from "./utils/ext";
 import storage from "./utils/storage";
 
 var popup = document.getElementById("app");
-storage.get('bookml', function(resp) {
+storage.get('bookml', function (resp) {
   if (resp.bookml) {
     renderList(resp.bookml);
   }
 });
 
 var optionsLink = document.querySelector(".js-options");
-optionsLink.addEventListener("click", function(e) {
+optionsLink.addEventListener("click", function (e) {
   e.preventDefault();
   ext.tabs.create({'url': ext.extension.getURL('options.html')});
 })
@@ -20,23 +20,26 @@ var renderList = (list) => {
     elListItem += `<button class="[ list-item ] [ btn-small ]" data-url="${value.url}">${value.title}</button>`
   })
   document.querySelector(".list").innerHTML = elListItem;
-  document.querySelector(".list-item").addEventListener("click", function(e) {
-    var bookMlUrl = e.target.getAttribute('data-url');
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', `${bookMlUrl}/raw`);
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        var bookMl = xhr.responseText;
-        ext.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          var activeTab = tabs[0];
-          chrome.tabs.executeScript(activeTab.id, { code: bookMl });
-        });
-      }
-      else {
-        console.log(xhr.status);
-      }
-    };
-    xhr.send();
+  document.querySelectorAll(".list-item").forEach((item) => {
+    item.addEventListener("click", function (e) {
+      var bookMlUrl = e.target.getAttribute('data-url');
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', `${bookMlUrl}`);
+      xhr.onload = function () {
+        if (xhr.status === 200) {
+          var bookMl = xhr.responseText;
+          console.log(bookMl);
+          ext.tabs.query({active: true, currentWindow: true}, function (tabs) {
+            var activeTab = tabs[0];
+            chrome.tabs.executeScript(activeTab.id, {code: bookMl});
+          });
+        } else {
+          console.log(xhr.status);
+        }
+      };
+      xhr.send();
+    })
   })
+
 }
 
